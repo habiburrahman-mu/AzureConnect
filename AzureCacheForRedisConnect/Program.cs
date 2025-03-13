ece970b36d174775263e1ce31315e0c40e27db59
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-03-14 00:48:56 +0600

    Updated azure cache for redis connect project
    Modified `Program.cs`
    to establish a connection to Redis, execute `PING`, set a
    value in the cache, and retrieve it, printing results to
    the console.

--- AzureCacheForRedisConnect/Program.cs
+++ AzureCacheForRedisConnect/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StackExchange.Redis;
+
+var connectionString = "your-redis-connection-string";
+
+using (var cache = ConnectionMultiplexer.Connect(connectionString))
+{
+    IDatabase database = cache.GetDatabase();
+
+    var result = await database.ExecuteAsync("PING");
+    Console.WriteLine($"PING = {result.Resp2Type}: {result}");
+    /// PING = SimpleString : PONG
+
+    string key = "mykey";
+    bool setValue = await database.StringSetAsync(key, "myvalue");
+    Console.WriteLine($"SET {key} = {setValue}");
+    /// SET mykey = True
+
+    var getValue = await database.StringGetAsync(key);
+    Console.WriteLine($"GET {key} = {getValue}");
+    /// GET mykey = myvalue
+}
