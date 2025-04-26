1785ba7f14d0b1aaa1b8fce87b0bb758aab783eb
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-04-26 13:40:26 +0600

    added project for service bus integraiton and wrote code for sender

---
+++ AzureBusServiceSender/Program.cs
@@ -0,0 +1,41 @@
+﻿// connection string to your Service Bus namespace
+using Azure.Messaging.ServiceBus;
+
+string connectionString = "<CONNECTION STRING>";
+
+// name of your Service Bus topic
+string queueName = "az204-queue";
+
+var client = new ServiceBusClient(connectionString);
+var sender = client.CreateSender(queueName);
+
+// create a batch 
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+for (int i = 1; i <= 3; i++)
+{
+    // try adding a message to the batch
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+    {
+        // if an exception occurs
+        throw new Exception($"Exception {i} has occurred.");
+    }
+}
+
+try
+{
+    // Use the producer client to send the batch of messages to the Service Bus queue
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of three messages has been published to the queue.");
+}
+finally
+{
+    // Calling DisposeAsync on client types is required to ensure that network
+    // resources and other unmanaged objects are properly cleaned up.
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+
+Console.WriteLine("Press any key to continue");
+Console.ReadKey();
